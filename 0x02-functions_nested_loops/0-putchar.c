9ff791df640bc6e98a834d3ef6e9f8ddcac78ef8
confidence <confidencestanley1999@gmail.com>
2022-10-23 05:47:24 -0700

    major

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by confidence
- * Return: 0
+ * Main - This script prints _putchar
+ * with a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,10 +11,10 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0)
+
+	return (0);
 }
